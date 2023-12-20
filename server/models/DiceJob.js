4b08859d43214dba4a6a8b9cc07c38e07b6f4305
MusaByte <75961076+ElijahAlly@users.noreply.github.com>
2023-12-19 20:44:45 -0500

    fixed lint errors

--- server/models/DiceJob.js
+++ server/models/DiceJob.js
@@ -1,55 +1,55 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const DiceJobSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    company: {
-        type: String,
-        required: true,
-    },
-    company_link: {
-        type: String,
-        required: false,
-    },
-    company_logo: {
-        type: String,
-        required: false,
-    },
-    location: {
-        type: String,
-        required: false,
-    },
-    job_link: {
-        type: String,
-        required: false,
-    },
-    employment_details: {
-        type: String,
-        required: false,
-    },
-    employment_pay: {
-        type: String,
-        required: false,
-    },
-    description_list: {
-        type: String,
-        required: false,
-    },
-    employment_skills: {
-        type: String,
-        required: false,
-    },
-    date_posted: {
-        type: String,
-        required: true,
-    },
-    date_updated: {
-        type: String,
-        required: false,
-    },
+  title: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  company_link: {
+    type: String,
+    required: false,
+  },
+  company_logo: {
+    type: String,
+    required: false,
+  },
+  location: {
+    type: String,
+    required: false,
+  },
+  job_link: {
+    type: String,
+    required: false,
+  },
+  employment_details: {
+    type: String,
+    required: false,
+  },
+  employment_pay: {
+    type: String,
+    required: false,
+  },
+  description_list: {
+    type: String,
+    required: false,
+  },
+  employment_skills: {
+    type: String,
+    required: false,
+  },
+  date_posted: {
+    type: String,
+    required: true,
+  },
+  date_updated: {
+    type: String,
+    required: false,
+  },
 });
 
-module.exports = DiceJob = mongoose.model('DiceJob', DiceJobSchema, 'dice_jobs');
+module.exports = mongoose.model("DiceJob", DiceJobSchema, "dice_jobs");
--- server/routes/api/dice.js
+++ server/routes/api/dice.js
@@ -6,32 +6,36 @@ const DiceJob = require("../../models/DiceJob");
 // needs...
 // in body: nothing
 // in params: nothing
-router.get('/', function (req, res) {
-    // console.log('id passed', req.params.id);
-    DiceJob.find()
-        .then((jobs) => { 
-            // console.log('dice job', jobs);
-            return res.json(jobs); 
-        })
-        .catch((err) => {
-            return res.status(404).json({ nojobsFound: 'No dice jobs found with that ID' });
-        });
+router.get("/", function (req, res) {
+  // console.log('id passed', req.params.id);
+  DiceJob.find()
+    .then((jobs) => {
+      // console.log('dice job', jobs);
+      return res.json(jobs);
+    })
+    .catch(() => {
+      return res
+        .status(404)
+        .json({ nojobsFound: "No dice jobs found with that ID" });
+    });
 });
 
 // get dice job by id
 // needs...
 // in body: nothing
 // in params: job_id
-router.get('/:id', function (req, res) {
-    // console.log('id passed', req.params.id);
-    DiceJob.findById(req.params.id)
-        .then((job) => { 
-            // console.log('dice job', job);
-            return res.json(job); 
-        })
-        .catch((err) => {
-            return res.status(404).json({ nojobFound: 'No dice job found with that ID' });
-        });
+router.get("/:id", function (req, res) {
+  // console.log('id passed', req.params.id);
+  DiceJob.findById(req.params.id)
+    .then((job) => {
+      // console.log('dice job', job);
+      return res.json(job);
+    })
+    .catch(() => {
+      return res
+        .status(404)
+        .json({ nojobFound: "No dice job found with that ID" });
+    });
 });
 
 module.exports = router;
